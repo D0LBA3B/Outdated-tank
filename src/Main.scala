5a48dc23c80aff51a01de18dfe0bf0369495207e
zcAntZuf <anthony.zufferey@zeacold.com>
2024-12-26 13:20:47 +0100

    First map implementation

--- src/Main.scala
+++ src/Main.scala
@@ -1,6 +1,21 @@
+import models.{Cell, Game, Terrain, Wall}
+
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
     println("We are ready for the war")
+
+    val game :Game = new Game(
+      name = "An epic battle",
+      grid = Array(
+          Array(new Cell(new Terrain()),new Cell(new Terrain()),new Cell(new Terrain()),new Cell(new Terrain()),new Cell(new Terrain())),
+          Array(new Cell(new Terrain()),new Cell(new Terrain()),new Cell(new Wall(1)),new Cell(new Terrain()),new Cell(new Terrain())),
+          Array(new Cell(new Terrain()),new Cell(new Wall(1)),new Cell(new Wall(1)),new Cell(new Wall(1)),new Cell(new Terrain())),
+          Array(new Cell(new Terrain()),new Cell(new Terrain()),new Cell(new Wall(1)),new Cell(new Terrain()),new Cell(new Terrain())),
+          Array(new Cell(new Terrain()),new Cell(new Terrain()),new Cell(new Terrain()),new Cell(new Terrain()),new Cell(new Terrain())),
+      )
+    )
+
+    game.drawGrid()
   }
 }
--- src/models/Game.scala
+++ src/models/Game.scala
@@ -1,5 +1,23 @@
 package models
 
-class Game(var grid: Array[Array[Cell]]){
-  var isOver = false
+import hevs.graphics.FunGraphics
+
+class Game(val name: String, private var grid: Array[Array[Cell]]){
+  private var cellSize :Int = 50; // Define de size of length TODO: Remove it later it only helps for debbugging
+  private var fg :FunGraphics = new FunGraphics(width = grid.length * cellSize, height = grid(0).length * cellSize)
+  var isOver: Boolean = false
+
+  def drawGrid(): Unit= {
+    // Grid of cell exploration
+    for(i <- grid.indices) {
+      for ((c, j) <- grid(i).zipWithIndex) {
+
+        //Draw square now 5x5
+        for (y <- 0 until cellSize; x <- 0 until cellSize){
+          fg.setColor(c.getColor)
+          fg.setPixel(x + j * cellSize, y + i * cellSize)
+        }
+      }
+    }
+  }
 }
