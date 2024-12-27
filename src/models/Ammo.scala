822bf06503e05932922631ae9007332afca2a734
zcAntZuf <anthony.zufferey@zeacold.com>
2024-12-27 11:19:26 +0100

    Add more attributs to ammo class

--- src/models/Ammo.scala
+++ src/models/Ammo.scala
@@ -1,5 +1,25 @@
 package models
 
-class Ammo(val damage: Int, val velocity: Int){
+import java.util.Date
 
+class Ammo(
+            private val damage: Int,
+            private var rebounce: Int,
+            private var posX: Int,
+            private var posY: Int,
+            private var angle: Int,
+            private var size: Int,
+            private val shotOn: Date
+          ) {
+
+  def hasHitWall(): Unit = {
+    // Reduce rebounce
+
+    // Change angle
+  }
+
+  def hasHitPlayer(): Unit = {
+    // Get player and reduce his HP
+    
+  }
 }
