1a22968faa38f4cb2b85171cae265a2fb9147a2a
zcAntZuf <anthony.zufferey@zeacold.com>
2024-12-28 12:24:40 +0100

    Refactor position are always integer value

--- Outdated-tank/src/main/scala/models/Position.scala
+++ Outdated-tank/src/main/scala/models/Position.scala
@@ -1,3 +1,3 @@
 package isc.game.outdatedtank.models
 
-case class Position(var x: Double, var y: Double)
+case class Position(var x: Int, var y: Int)
--- Outdated-tank/src/main/scala/models/Tank.scala
+++ Outdated-tank/src/main/scala/models/Tank.scala
@@ -8,7 +8,7 @@ class Tank(var position: Position,
 
   val projectiles: collection.mutable.ListBuffer[Ammo] = collection.mutable.ListBuffer.empty
 
-  def move(dx: Double, dy: Double): Unit = {
+  def move(dx: Int, dy: Int): Unit = {
     position.x += dx
     position.y += dy
   }
