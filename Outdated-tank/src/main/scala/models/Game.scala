a2ca5eaad47007943089b42aaa70969289cf2dfd
DOLBAEB <clementfavre755@gmail.com>
2024-12-30 20:37:51 +0100

    feat: menu enhancement

--- Outdated-tank/src/main/scala/models/Game.scala
+++ Outdated-tank/src/main/scala/models/Game.scala
@@ -1,8 +1,13 @@
 package isc.game.outdatedtank.models
 
-import hevs.graphics.FunGraphics
+import hevs.graphics.{FunGraphics, ImageGraphics}
+import hevs.graphics.utils.GraphicsBitmap
 import isc.game.outdatedtank.MapReader
-import java.awt.Dimension
+
+import java.awt.{Color, Desktop, Dimension}
+import java.io.{File, FileInputStream}
+import java.net.URI
+import javax.imageio.ImageIO
 
 object Game {
   private var gameInstance: Game = null
@@ -18,15 +23,6 @@ object Game {
   def getWindow(width: Int = 800, height: Int = 600, title: String = "Outdated Tank"): FunGraphics = {
     if (gameWindow == null) {
       gameWindow = new FunGraphics(width, height, title)
-    } else if (width > 0 && height > 0 && (gameWindow.mainFrame.getWidth != width || gameWindow.mainFrame.getHeight != height)) {
-      // window content resizing not working? TODO
-      //gameWindow.mainFrame.setSize(width, height)
-      //val contentPane = gameWindow.mainFrame.getContentPane
-      //contentPane.setSize(width, height)
-      //contentPane.repaint()
-      //gameWindow.clear()
-      gameWindow.mainFrame.dispose()
-      gameWindow = new FunGraphics(width, height, title)
     }
     gameWindow
   }
@@ -36,39 +32,69 @@ class Game private(val config: GameConfig) {
   var isGameOver: Boolean = true
 
   def start(): Unit = {
-    //showMenu()
-    launchGame()
+    showMenu()
   }
 
   private def showMenu(): Unit = {
-      val menuWidth = 300
-      val menuHeight = 200
-      val buttonX = 100
-      val buttonY = 100
-      val buttonWidth = 100
-      val buttonHeight = 50
+    val menuWidth = 750
+    val menuHeight = 750
+    val buttonWidth: Int = 200
+    val buttonHeight: Int = 50
+    val buttonSpacing: Int = 20
 
-      val menuWindow = Game.getWindow(menuWidth, menuHeight)
-      menuWindow.clear()
+    val menuWindow = Game.getWindow(menuWidth, menuHeight)
+    menuWindow.clear(new Color(140, 129, 107, 255))
 
-      menuWindow.setColor(java.awt.Color.GREEN)
+    // Logo
+    val bufferedLogo = ImageIO.read(new FileInputStream(new File("./res/logo.png")))
+    val logoBitmap = new GraphicsBitmap("")
+    logoBitmap.mBitmap = bufferedLogo
+    val logoX = (menuWidth - bufferedLogo.getWidth) / 2
+    val logoY = menuHeight / 4 - bufferedLogo.getHeight / 2
+    menuWindow.drawPicture(logoX, logoY, logoBitmap)
+
+    // Buttons
+    val buttonLabels = List("FIGHT !", "OPTIONS", "CREDITS", "EXIT")
+    val startY = logoY + bufferedLogo.getHeight  + 2 * buttonHeight
+    buttonLabels.zipWithIndex.foreach { case (label, index) =>
+      val buttonX: Int = (menuWidth - buttonWidth) / 2
+      val buttonY: Int = startY + index * (buttonHeight + buttonSpacing)
+
+      // background
+      menuWindow.setColor(Color.LIGHT_GRAY)
       menuWindow.drawFillRect(buttonX, buttonY, buttonWidth, buttonHeight)
 
-      menuWindow.setColor(java.awt.Color.WHITE)
-      menuWindow.drawString(buttonX + 20, buttonY + 30, "FIGHT !")
+      // label
+      menuWindow.setColor(Color.WHITE)
+      val stringSize = menuWindow.getStringSize(label)
+      val labelX = buttonX + (buttonWidth - stringSize.getWidth.toInt - 75) / 2
+      val labelY = buttonY + (buttonHeight + stringSize.getHeight.toInt + 5) / 2
+      menuWindow.drawString(labelX, labelY, label, Color.WHITE, 30)
+    }
 
-      menuWindow.mainFrame.addMouseListener(new java.awt.event.MouseAdapter {
-        override def mouseClicked(e: java.awt.event.MouseEvent): Unit = {
-          val mouseX = e.getX
-          val mouseY = e.getY
+    // Menu events
+    menuWindow.mainFrame.addMouseListener(new java.awt.event.MouseAdapter {
+      override def mouseClicked(e: java.awt.event.MouseEvent): Unit = {
+        val mouseX = e.getX
+        val mouseY = e.getY
+
+        buttonLabels.zipWithIndex.foreach { case (label, index) =>
+          val buttonX: Int = (menuWidth - buttonWidth) / 2
+          val buttonY: Int = startY + index * (buttonHeight + buttonSpacing)
 
           if (mouseX >= buttonX && mouseX <= buttonX + buttonWidth &&
-              mouseY >= buttonY && mouseY <= buttonY + buttonHeight) {
-            println("LAUNCH THE GAME...")
-            launchGame()
+            mouseY >= buttonY && mouseY <= buttonY + buttonHeight) {
+            label match {
+              case "FIGHT !" => launchGame()
+              case "OPTIONS" => println("TODO")
+              case "CREDITS" => Desktop.getDesktop.browse(new URI("https://github.com/D0LBA3B/Outdated-tank/"))
+              case "EXIT" => System.exit(0)
+              case _ =>
+            }
           }
         }
-      })
+      }
+    })
   }
 
   private def launchGame(): Unit = {
